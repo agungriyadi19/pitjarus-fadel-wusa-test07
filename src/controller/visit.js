8bd0a500da419f92e65a6b520ada1efe55153c19
agungriyadi <agus050577@gmail.com>
2022-09-19 12:57:40 +0700

    soal A, B dan tambahan

--- src/controller/visit.js
+++ src/controller/visit.js
@@ -1,6 +1,7 @@
 const catchAsync = require('../util/catchAsync');
 const db = require('../models')
-
+const axios = require('axios');
+db.category.hasMany(db.report_display, { as: "json_paths", foreignKey: 'category_id' });
 
 const getVisit = catchAsync(async (req, res) => {
     const {visit_id: visitId} = req.params
@@ -21,7 +22,40 @@ const getVisit = catchAsync(async (req, res) => {
 
 const getReportDisplay = catchAsync(async (req, res) => {
     /** logic here */
+    const visit_id = req.body.visit_id
 
+    //Query report
+    const getReport = await db.sequelize.query(
+        `SELECT 
+        rd.visit_id, s.id as store_id, s.name as store_name, 
+        sr.id as surveyor_id, sr.username as surveyor_name  
+        FROM report_display as rd 
+        LEFT JOIN store as s on rd.store_id = s.id
+        LEFT JOIN surveyor as sr on rd.surveyor_id = sr.id 
+        WHERE rd.visit_id = :visit_id
+        GROUP BY visit_id`, {
+            replacements : {
+                visit_id: visit_id
+            },
+            type: db.sequelize.QueryTypes.SELECT
+        }
+    );
+
+    //Query Display
+    const getDisplay = await db.category.findAll({
+        attributes: [['id','category_id'], ['name', 'category_name']],
+        include: [{
+            where: {
+                visit_id: visit_id
+            },
+            model: db.report_display,
+            as: "json_paths",
+            attributes: [
+                ['json_path', 'path'],
+            ],
+        }],
+    })
+    getReport[0].displays=getDisplay
     /* contoh output */
     const expectedOutput = {
         visit_id: "V.26.865.22081208343138",
@@ -48,12 +82,62 @@ const getReportDisplay = catchAsync(async (req, res) => {
             }
         ]
     }
-    res.status(200).json(expectedOutput)
+    res.status(200).json(getReport)
 })
 
 const getReportProduct = catchAsync(async (req, res) => {
     /** logic here */
+    let products = []
+    let product = []
+    const count = {};
+    const visit_id = req.body.visit_id
+    //Query JSON Path
+    const getJsonPath = await db.report_display.findAll({
+        where: {
+            visit_id: visit_id
+        },
+        attributes: [
+            'json_path',
+        ],
+    })
 
+    // Get label product
+    for (let index = 0; index < getJsonPath.length; index++) {
+        const element = getJsonPath[index].json_path;
+        const getData = await axios.get(element)
+        for (let index = 0; index < getData.data.length; index++) {
+            const element = getData.data[index].object_name;
+            product.push(element);
+        }
+    }
+    //Count product
+    product.forEach(element => {
+        count[element] = (count[element] || 0) + 1;
+    });
+
+    // Query product id
+    let productName = Object.keys(count)
+    for (let index = 0; index < productName.length; index++) {
+        const element = productName[index];
+        const value = Object.values(count)[index];
+        const getProductID = await db.product.findOne({
+            where: {
+                label: element
+            },
+            attributes: [
+                'id',
+            ],
+        })
+        let data = {
+            product_id: getProductID.id,
+            jumlah: value,
+        }
+        products.push(data);
+    }
+    let response = {
+        visit_id: visit_id,
+        products: products
+    }
     /* contoh output */
     const expectedOutput = {
         visit_id: "V.26.865.22081208343138",
@@ -64,11 +148,24 @@ const getReportProduct = catchAsync(async (req, res) => {
             {product_id: 5, jumlah: 6},
         ]
     }
-    res.status(200).json(expectedOutput)
+    res.status(200).json(response)
 })
 
 const batchReportProduct = catchAsync(async (req, res) => {
     /** logic here */
+    const visit_id = req.body.visit_id
+    let { PORT } = process.env
+    let getReportProduct = await axios.post(`http://localhost:${ PORT }/product-visit`,{visit_id: visit_id})
+    
+    const reportProducts = getReportProduct.data.products.map(product => {
+        return {
+            visit_id: visit_id,
+            product_id: product.product_id,
+            jumlah_product: product.jumlah
+        }
+    })
+    db.report_product.bulkCreate(reportProducts);
+
     res.status(200).json({
         status: "OK",
         message: "batch success"
